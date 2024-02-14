b4c26ef27cbb3ca3d56d59774bea4ea1c0db7983
Vansh Goyal <135618994+VanshGoyal000@users.noreply.github.com>
2024-02-14 12:10:01 +0530

    first commit

---
+++ input_string.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    
+    //Without Spaces
+
+    char c[200];
+    printf("Enter String : ");
+    scanf("%s" , &c);
+    printf("Entered String is");
+    printf("%s" , c);
+
+    //With Spaces
+
+    
+
+    return 0;
+}
